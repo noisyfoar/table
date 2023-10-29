b2d942bfbbf72050ec58a43b13bd59d2cdf13fd1
saladd <spad1239@gmail.com>
2023-10-30 01:40:09 +0500

    осталось сделать правильную загрузку файлов и правильное изменение размеров. Исправил работу наблюдателя

--- hdf.py
+++ hdf.py
@@ -4,10 +4,15 @@ import h5py
 DATAS = 'data'
 OBSERVERS = 'observers'
 TYPES = 'types'
+TABLE = 'table'
 
 
 # обязательно добавить считывание количества строк и столбцов при открытии файла
-# в obsevers в строке указаны столбцы, а в столбцах указаны наблюдатели
+
+# в observers в строках указаны столбцы, а в столбцах указаны наблюдатели
+
+# сделать проверку, если существует, иначе создать новый
+
 class Hdf:
     def __init__(self, filepath):
         self.filepath = filepath
@@ -19,27 +24,38 @@ class Hdf:
             data = f[DATAS][:]
             return data
 
-    def write_cell(self, row, object_column, data):
-        with h5py.File(self.filepath, 'w') as f:
-            dataset = f[DATAS]
-            dataset[row, object_column] = data
+    def show_observers(self):
+        with h5py.File(self.filepath, 'r') as f:
+            observers = f[OBSERVERS][:]
+            return observers
+
+    def show_types(self):
+        with h5py.File(self.filepath, 'r') as f:
+            types = f[TYPES][:]
+            return types
+
+    def write_cell(self, row, object_column, value):
+        with h5py.File(self.filepath, 'r+') as f:
+            data = f[DATAS]
+            data[row, object_column] = value
             # оповещаем всех наблюдателей, что столбец изменился
             self.notify_all(object_column)
 
     def update_observer(self, column_object, column_observer):
-        with h5py.File(self.filepath, 'r+') as f:
+        check = False
+        with h5py.File(self.filepath, 'r') as f:
             observers = f[OBSERVERS][:]
+        if observers[column_object, column_observer] == 0:
+            observers[column_object, column_observer] = 1
+            visited = set()
+            # проверка на цикличность для того, чтобы наблюдатель не следил за своими обновлениями
+            # если цикла нет, то происходит возвращение из функции, иначе не добавляем в список наблюдателя
+            if self.dfs(visited, observers, column_observer) is None:
+                check = True
 
-            if observers[column_object, column_observer] == 0:
-                observers[column_object, column_observer] = 1
-                visited = set()
-                # проверка на цикличность для того, чтобы наблюдатель не следил за своими обновлениями
-                if self.dfs(visited, observers, column_observer) is None:
-                    return
-                # если цикла нет, то происходит возвращение из функции, иначе не добавляем в список наблюдателя
-                observers[column_object, column_observer] = 0
-            else:
-                observers[column_object, column_observer] = 0
+        with h5py.File(self.filepath, 'r+') as f:
+            observers = f[OBSERVERS]
+            observers[column_object, column_observer] = int(check)
 
     def update_type(self, column):
         with h5py.File(self.filepath, 'w') as f:
@@ -49,13 +65,13 @@ class Hdf:
     def create_empty_table(self):
         with h5py.File(self.filepath, 'w') as f:
             arr = np.zeros(shape=(self.rows, self.columns))
-            data = f.create_dataset(DATAS, data=arr)
+            data = f.create_dataset(DATAS, data=arr, dtype='i8')
 
             arr = np.zeros(10)
-            f.create_dataset(TYPES, data=arr)
+            f.create_dataset(TYPES, data=arr, dtype='i8')
 
             arr = np.zeros(shape=(self.columns, self.columns))
-            f.create_dataset(OBSERVERS, data=arr)
+            f.create_dataset(OBSERVERS, data=arr, dtype='i8')
             # observers[:] = arr[:] если верхние две строки не заработают
             return data
 
@@ -69,14 +85,16 @@ class Hdf:
                     objects.add(column)
             if len(objects) == 0:
                 return
-            arr = []
+            arr = [0]*self.columns
             # так как мы собрали список всех наблюдателей, то мы можем уже собрать данные с них
             data = f[DATAS]
             for column in objects:
                 for row in range(self.rows):
                     arr[row] += data[row, column]
+            # нужно в цикле пройтись по всем наблюдателем, так как их может быть несколько
+
             # запишем эти данные в колонку
-            data[row, observer_column] = arr[row]
+            data[0, observer_column] = arr[0]
             for row in range(1, self.rows):
                 data[row, observer_column] = arr[row] + data[row - 1, observer_column]
         self.notify_all(observer_column)
@@ -91,26 +109,28 @@ class Hdf:
     # вспомогательная функция для нахождения цикличности для наблюдателя, если возвращает 1, то цикл найден если None, то нет
     def dfs(self, visited, observers, observer):
         visited.add(observer)
-        for neighbor in range(observers[:, observer]):
-            if neighbor not in visited:
-                self.dfs(visited, observers, neighbor)
-            else:
-                return 1
+        for neighbor in range(self.columns):
+            if observers[observer, neighbor] == 1:
+                if neighbor not in visited:
+                    if self.dfs(visited, observers, neighbor) is not None:
+                        return -1
+                else:
+                    return -1
 
     def resize_data(self, new_rows=None, new_columns=None):
-        old_columns = self.columns
-        old_rows = self.rows
+        check_rows = False  # заполнить новые строки, учитывая старые столбцы (для DATAS)
+        check_columns = False  # заполнить новые столбцы, учитывая старые строки (для DATAS, OBSERVERS, TYPES)
+        check_additional = False  # заполнить оставшуюся часть данных (для DATAS)
+        # сделаем проверки для того, чтобы после изменения размеров данных заполнить новые части нулями(для DATAS, OBSERVERS, TYPES), при обрезании данных это не понадобится
         if new_rows is not None:
-            # заполняем новую часть нулями(для DATAS, OBSERVERS, TYPES), при обрезании данных это не понадобится
-            if new_rows > old_rows:
-                arr = np.zeros((new_rows - old_rows, old_columns))
-                with h5py.File(self.filepath, 'w') as f:
-                    dataset = f[DATAS]
-
+            check_rows = new_rows > self.rows
+        else:
+            new_rows = self.rows
         if new_columns is not None:
-            a = 1
-        if (new_rows is not None) and (new_columns is not None):
-            a = 1
+            check_columns = new_columns > self.columns
+        else:
+            new_columns = self.columns
+        check_additional = check_columns and check_rows
 
         with h5py.File(self.filepath, 'w') as f:
             dataset = f[DATAS]
